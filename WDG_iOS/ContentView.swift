6d3eb1a701ec8cd28e7d3edcbf753b3ebfb43356
chanwoong1 <jcw7468@gmail.com>
2023-10-27 16:56:37 +0900

    Initial Commit

---
+++ WDG_iOS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WDG_iOS
+//
+//  Created by 정찬웅 on 10/27/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
