01abbf8dd14bbd051eaa7a93b7341669a4645604
chanwoong1 <jcw7468@gmail.com>
2023-10-27 16:56:37 +0900

    Initial Commit

---
+++ WDG_iOS/View/LoginView.swift
@@ -0,0 +1,8 @@
+//
+//  LoginView.swift
+//  WDG_iOS
+//
+//  Created by 정찬웅 on 10/27/23.
+//
+
+import Foundation
---
+++ WDG_iOS/WDG_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  WDG_iOSApp.swift
+//  WDG_iOS
+//
+//  Created by 정찬웅 on 10/27/23.
+//
+
+import SwiftUI
+
+@main
+struct WDG_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
