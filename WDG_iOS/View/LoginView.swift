aadb76f3e5a5c3d131b3956fcd176e2998b07831
chanwoong1 <jcw7468@gmail.com>
2023-10-27 17:39:48 +0900

    Feat: Login Logo 생성

--- WDG_iOS/View/LoginView.swift
+++ WDG_iOS/View/LoginView.swift
@@ -5,4 +5,36 @@
 //  Created by 정찬웅 on 10/27/23.
 //
 
-import Foundation
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        ZStack {
+            Color.black
+                .ignoresSafeArea()
+            WDGLogoView()
+        }
+    }
+}
+
+struct WDGLogoView: View {
+    var body: some View {
+        HStack(spacing: -10) {
+            Text("W")
+                .font(Font.custom("Dela Gothic One", size: 68))
+                .foregroundColor(.white)
+            Text("D")
+                .font(Font.custom("Dela Gothic One", size: 68))
+                .foregroundColor(.white)
+            Text("G")
+                .font(Font.custom("Dela Gothic One", size: 68))
+                .foregroundColor(.white)
+        }
+    }
+}
+
+struct LoginView_Preview: PreviewProvider {
+    static var previews: some View {
+        LoginView()
+    }
+}
