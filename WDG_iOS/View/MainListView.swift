af1dde5a48b2d5b441bba38b1aaffda6859c0dff
chanwoong1 <jcw7468@gmail.com>
2023-10-28 16:16:24 +0900

    Feat: 하단 탭 추가

--- WDG_iOS/View/MainListView.swift
+++ WDG_iOS/View/MainListView.swift
@@ -8,21 +8,9 @@
 import SwiftUI
 
 struct MainListView: View {
-    @StateObject var authKakao: AuthKakao = AuthKakao()
     var body: some View {
-        Group {
-            if authKakao.isLoggedIn {
-                // 사용자가 로그인한 경우 표시될 뷰
                 VStack {
                     Text("Main List")
-                    Button("KAKAO LOGOUT", action: {
-                        authKakao.handleKakaoLogout()
-                    })
                 }
-            } else {
-                // 사용자가 로그인하지 않은 경우 LoginView 표시
-                LoginView(authKakao: authKakao)
-            }
-        }
     }
 }
---
+++ WDG_iOS/View/PostView.swift
@@ -0,0 +1,18 @@
+//
+//  PostView.swift
+//  WDG_iOS
+//
+//  Created by 정찬웅 on 10/28/23.
+//
+
+import SwiftUI
+
+struct PostView: View {
+    var body: some View {
+        Text("Post")
+    }
+}
+
+//#Preview {
+//    PostView()
+//}
