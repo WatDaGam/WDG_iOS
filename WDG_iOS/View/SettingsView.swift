5cd67d2d7fc5fe4be8c57e9227351528bffae8a7
chanwoong1 <jcw7468@gmail.com>
2023-11-02 16:53:04 +0900

    Fix: alert이 두 개 이상일 경우 동작하지 않던 이전 방식 수정

--- WDG_iOS/View/SettingsView.swift
+++ WDG_iOS/View/SettingsView.swift
@@ -9,44 +9,53 @@ import SwiftUI
 
 struct SettingsView: View {
     @ObservedObject var authModel: AuthModel
-    @State private var isLogout: Bool = false
-    @State private var isRemove: Bool = false
+    @State private var alertType: AlertType?
+    enum AlertType: Identifiable {
+        case logout
+        case removeAccount
+        var id: Int {
+            switch self {
+            case .logout:
+                return 0
+            case .removeAccount:
+                return 1
+            }
+        }
+    }
     var body: some View {
-        NavigationView {  // NavigationView를 추가
+        NavigationView {
             ZStack {
-                Color.white.edgesIgnoringSafeArea(.all)  // 전체 화면에 흰색 배경 적용
+                Color.white.edgesIgnoringSafeArea(.all)
                 List {
                     Button("프로필", action: { print("profile clicked!") })
                     Button("내 작성 목록", action: { print("my list clicked!") })
-                    Button("로그아웃", action: { isLogout = true })
-                    Button("회원탈퇴", action: { isRemove = true })
+                    Button("로그아웃", action: { alertType = .logout })
+                    Button("회원탈퇴", action: { alertType = .removeAccount })
                 }
-                .listStyle(.plain)  // 리스트 스타일을 평범한 스타일로 설정
+                .listStyle(.plain)
             }
-            .navigationBarTitle("마이페이지", displayMode: .inline)  // 상단 헤더 타이틀 설정
-            .alert(isPresented: $isLogout) {
-                Alert(
-                    title: Text("로그아웃"),
-                    message: Text("로그아웃 시 로그인 화면으로 이동합니다."),
-                    primaryButton: .destructive(Text("예")) {
-                        // "예"를 선택했을 때의 동작
-                        // 토큰 삭제 및 로그아웃 처리
-                        authModel.handleKakaoLogout()
-                    },
-                    secondaryButton: .cancel(Text("아니오"))
-                )
-            }
-            .alert(isPresented: $isRemove) {
-                Alert(
-                    title: Text("회원탈퇴"),
-                    message: Text("회원탈퇴 시 모든 데이터가 삭제됩니다."),
-                    primaryButton: .destructive(Text("탈퇴")) {
-                        // "예"를 선택했을 때의 동작
-                        // 토큰 삭제 및 로그아웃 처리
-                        authModel.handleKakaoLogout()
-                    },
-                    secondaryButton: .cancel(Text("취소"))
-                )
+            .navigationBarTitle("마이페이지", displayMode: .inline)
+            .alert(item: $alertType) { type in
+                switch type {
+                case .logout:
+                    return Alert(
+                        title: Text("로그아웃"),
+                        message: Text("로그아웃 시 로그인 화면으로 이동합니다."),
+                        primaryButton: .destructive(Text("예")) {
+                            authModel.handleKakaoLogout()
+                        },
+                        secondaryButton: .cancel(Text("아니오"))
+                    )
+                case .removeAccount:
+                    return Alert(
+                        title: Text("회원탈퇴"),
+                        message: Text("회원탈퇴 시 모든 데이터가 삭제됩니다."),
+                        primaryButton: .destructive(Text("탈퇴")) {
+                            authModel.deleteAccountWithKakao()
+                        },
+                        secondaryButton: .cancel(Text("취소"))
+                    )
+                }
             }
         }
     }
