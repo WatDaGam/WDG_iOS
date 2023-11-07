a5948c9fc6374ef33767bd33b5868ad919f5ca2a
chanwoong1 <jcw7468@gmail.com>
2023-11-07 09:54:48 +0900

    Feat: 메인 리스트 뷰 개발 시작

--- WDG_iOS/View/MainListView.swift
+++ WDG_iOS/View/MainListView.swift
@@ -10,15 +10,50 @@ import SwiftUI
 struct MainListView: View {
     @EnvironmentObject var authModel: AuthModel
     @EnvironmentObject var tokenModel: TokenModel
+    @EnvironmentObject var postModel: PostModel
     var body: some View {
         VStack {
-            Button(action: {
-                Task {
-                    await tokenModel.validateToken(authModel: authModel)
+            Header()
+            List {
+                ForEach(postModel.posts) { post in
+                    Post(post: post)
                 }
-            }, label: {
-                Text("토큰 재발급 테스트")
-            })
+            }
+            .listStyle(.plain)
         }
+    }
+}
+
+struct Header: View {
+    var body: some View {
+        HStack {
+            WDGLogoView(size: 34, spacing: -5, mode: true)
+//            Image(systemName: "heart")
+            Spacer()
+            VStack {
+                Spacer()
+                Text("서초동")
+                    .fontWeight(.bold)
+                    .foregroundColor(.white)
+                    .font(.title2)
+                Spacer()
+                Text("대충 위치")
+                    .foregroundColor(.white)
+                    .font(.caption)
+                Spacer()
+            }
+            Spacer()
+            WDGLogoView(size: 34, spacing: -5, mode: false)
+        }
+        .frame(height: 80)
+        .background(Rectangle().foregroundColor(.black))
+    }
+}
+
+struct MainListViewPreviews: PreviewProvider {
+    static var previews: some View {
+        let postModel = PostModel()
+        MainListView()
+            .environmentObject(postModel)
     }
 }
